c38f4139318098628ef9351394db7c169eef04bf
mariamanurungg <mariamanurungg@gmail.com>
2022-12-23 16:59:04 +0800

    first commit~

---
+++ js/js-quiz-2.js
@@ -0,0 +1,89 @@
+function result()
+{
+	var score=0;
+	if(document.getElementById('correct1').checked)
+		{	score++;
+		}
+	if(document.getElementById('correct2').checked)
+		{	score++;
+		}
+	if(document.getElementById('correct3').checked)
+		{	score++;
+		}
+    alert("Your score is:"+score);
+ // to print on browser
+    document.write("Nilai mu adalah "+score);
+}
+
+const quizData = [
+    {
+        question: "1. Manakah Planet terbesar didalam tata surya kita?",
+        a: "Jupiter",
+        b: "Venus",
+        c: "Mars",
+        d: "Neptunus",
+        correct: "a",
+    },
+    {
+        question: "2. Dibawah ini yang manakah benda angkasa yang kecil dan padat yang hampir seluruh isinya terbentuk dari gas dan debu yang membeku?",
+        a: "Asteroid",
+        b: "Bulan",
+        c: "Komet",
+        d: "Meteor",
+        correct: "c",
+    },
+];
+const quiz= document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
+let currentQuiz = 0
+let score = 0
+loadQuiz()
+function loadQuiz() {
+    deselectAnswers()
+    const currentQuizData = quizData[currentQuiz]
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+}
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+}
+function getSelected() {
+    let answer
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+    return answer
+}
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    if(answer) {
+       if(answer === quizData[currentQuiz].correct) {
+           score++
+       }
+       currentQuiz++
+       if(currentQuiz < quizData.length) {
+           loadQuiz()
+       } else {
+           quiz.innerHTML = `
+           <h2 style="margin-right : 20px;">Jawaban kamu ${score}/${quizData.length} Pertanyaan yang benar</h2> </br>
+           <button style="margin-right : 20px; border-radius: 20px; padding: 10px;" onclick="location.reload()">Reload</button>
+           <button style="margin-right : 20px; border-radius: 20px; padding: 10px;" onclick="newTab()">Halaman Utama</button>
+           `
+       }
+    }
+})
+
+function newTab() {
+    window.location.href = "index.html";
+}
